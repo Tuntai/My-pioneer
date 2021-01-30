6237b177efe6322b3619470215bda95871db30b4
Aparajita Paul <57225563+Tuntai@users.noreply.github.com>
2021-01-31 01:59:18 +0530

    Add files via upload

---
+++ js/resource.js
@@ -0,0 +1,14 @@
+let link = 'https://www.coursera.org/learn/';
+let home = '/home/';
+
+
+document.getElementById("overview_591").href = link+'cybersecurity'+home; 
+document.getElementById("overview_592").href = link+'machine-learning'+home;
+document.getElementById("overview_593").href = link+'python'+home;
+document.getElementById("overview_594").href = link+'javascript'+home;
+document.getElementById("overview_595").href = link+'financial-markets-global'+home;
+document.getElementById("overview_596").href = link+'ai-for-everyone'+home;
+document.getElementById("overview_549").href = link+'what-is-datascience'+home;
+document.getElementById("overview_581").href = link+'computer-networking'+home;
+document.getElementById("overview_547").href = link+'computational-thinking-problem-solving'+home;
+document.getElementById("overview_515").href = link+'user-experience-design'+home;
